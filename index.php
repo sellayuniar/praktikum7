cf9739a0bcb9fe19632643955a5ba240d534161a
sellayuniar <79758456+sellayuniar@users.noreply.github.com>
2021-04-26 12:29:56 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
-<html>
-<head>
-	<title>Membuat Login Dengan PHP</title>	
-</head>
-<body>
-	<h3>Halaman Login </h3>
-	<form method="post" action="login.php" >		
-		<table>
-			<tr>
-				<td>Username</td>
-				<td><input type="text" name="username"></td>
-			</tr>
-			<tr>
-				<td>Password</td>
-				<td><input type="password" name="password"></td>
-			</tr>
-			<tr>
-				<td></td>
-				<td><input type="submit" name="login" value="Log In"></td>
-			</tr>
-		</table>
-	</form>
-</body>
-</html>
+<h2> List Mahasiswa</h2>
+<table border="1"><!--Membuat tabel yang nantinya berisi data mahasiswa -->
+	<tr> <th> NO </th> <th> NIM </th> <th> NAMA </th> <th> GENDER </th> <th> JURUSAN </th> <th> ALAMAT </th> 
+	<?php 
+	include 'koneksi.php'; //memasukan file koneksi.php
+	$mahasiswa = mysqli_query($koneksi, "SELECT * from mahasiswa"); //membuat variabel mahasiswa yang menyimpan variable koneksi dan query select
+	$no = 1; //membuat variabel no yang menyimpan nilai 1
+	foreach ($mahasiswa as $row) { //untuk membuat perulangan 
+		$jenis_kelamin = $row['jenis_kelamin']=='P'?'Perempuan':'Laki Laki'; //membuat variabel jenis kelamin
+		echo //menampilkan data-data dari database yang sudah tersambung
+		"<tr> 
+		<td> $no </td>
+		<td>".$row['nim']." </td>
+		<td>".$row['nama']." </td>
+		<td>".$jenis_kelamin." </td>
+		<td>".$row['jurusan']." </td>
+		<td>".$row['alamat']." </td>
+			</tr>";
+		$no++; //nilai variabel no akan bertambah sesuai dengan berapa kali perulangan berlangsung
+	}
+	?>
+	
+</table>
